7b75294046e64b290aeb40bd0050f342ad1c8278
Dan Mincu <danmincu@gmail.com>
2021-02-22 23:11:04 -0500

    all good

--- TreeViewer/MainWindow.xaml.cs
+++ TreeViewer/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
+using TestApp.Tree;
 
 namespace TestApp
 {
@@ -10,6 +12,58 @@ namespace TestApp
 		public Window1()
 		{
 			InitializeComponent();
+			var model = NodeModel.LoadFromFile("dept.txt");
+			_treeList.Model = model;
 		}
-	}
+
+
+		private void Toggle_Click(object sender, RoutedEventArgs e)
+		{
+			foreach (var node in _treeList.SelectedNodes)
+				if (node.IsExpandable)
+					node.IsExpanded = !node.IsExpanded;
+		}
+
+		private void Add_Click(object sender, RoutedEventArgs e)
+		{
+			if (_treeList.SelectedNode != null)
+			{
+				var p = new Node() { Name = "New node" };
+				(_treeList.SelectedNode.Tag as Node).Children.Add(p);
+				_treeList.SelectedNode.IsExpanded = true;
+			}
+		}
+
+		private void Toggle_Click_All(object sender, RoutedEventArgs e)
+		{
+			foreach (var node in _treeList.SelectedNodes)
+			{
+				Expand(node);
+			}
+		}
+
+		private TreeNode Expand(TreeNode node)
+		{
+			if (node.IsExpandable)
+				node.IsExpanded = true;//!node.IsExpanded;
+
+			foreach (var item in node.AllVisibleChildren)
+			{
+				Expand(item);
+			}
+
+			return node;
+
+		}
+
+        private void Load_File(object sender, RoutedEventArgs e)
+        {
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == true)
+			{
+     			var model = NodeModel.LoadFromFile(openFileDialog.FileName);
+				_treeList.Model = model;
+			}
+		}
+    }
 }
